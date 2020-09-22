692e178d2a566a6389ce3cce028791a87d995ef1
nayankhatua <nayankhatua@users.noreply.github.com>
2020-09-22 11:49:47 +0530

    Update hello.java
    Add another orint statement

--- hello.java
+++ hello.java
@@ -5,6 +5,7 @@ public class hello{
 		for (int i=1;i<=10;i++){
 			System.out.println("Hello world...."+i);
 			System.out.println("This is Integration of Jenkin and git...."+i);
+			System.out.println("My Name Is Nayan"+i);
 
 		}
 
